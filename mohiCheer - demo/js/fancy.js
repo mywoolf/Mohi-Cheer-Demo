bc6b09708913e90f3535fb7b72835b1b696c010a
Melissa Woolf <mywoolf@comcast.net>
2014-09-19 14:26:00 -0600

    First Post of Mohi Cheer Demo

---
+++ mohiCheer - demo/js/fancy.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    
+    //<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+        
+        
+    <!-- FancyBox -->
+        <script src="js/fancybox/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script src="js/fancybox/jquery.fancybox.js"></script>
+        <script src="js/fancybox/jquery.fancybox-buttons.js"></script>
+        <script src="js/fancybox/jquery.fancybox-thumbs.js"></script>
+        <script src="js/fancybox/jquery.easing-1.3.pack.js"></script>
+        <script src="js/fancybox/jquery.mousewheel-3.0.6.pack.js"></script>
+        
+        <script type="text/javascript">
+                $(".various").fancybox({
+                    maxWidth    : 800,
+                    maxHeight   : 600,
+                    fitToView   : false,
+                    width       : '70%',
+                    height      : '70%',
+                    autoSize    : false,
+                    closeClick  : false,
+                    openEffect  : 'elastic',
+                    closeEffect : 'none'
+                });
+        
+        </script>
+});
+        
---
+++ mohiCheer - demo/js/slider.js
@@ -0,0 +1,26 @@
+//$(document).ready(function() {
+//the follwing line is the abbreviated version of the above code
+$(function() {
+  $('#slider').anythingSlider();
+}); 
+
+
+
+
+
+
+/*TRY THIS ANIMATION ON AN OBJECT
+$("#slider").click (function(){
+   $(this).animate({  height:'600px',
+                      width:'600px',
+                      left : '-11px'
+                    })
+                            });
+
+$("#slider").mouseout(function(){
+   $(this).animate({
+                      height:'178px',
+                      width:'178px',
+                      left : '0px'
+                    })
+*/
